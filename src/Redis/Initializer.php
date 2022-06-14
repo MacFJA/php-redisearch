dbf3e4a5baa833ab226b3ec625b87f854e207283
Vincent Radstake <vlradstake@gmail.com>
2022-06-14 17:00:17 +0200

    Update Initializer.php

--- src/Redis/Initializer.php
+++ src/Redis/Initializer.php
@@ -142,7 +142,7 @@ class Initializer
         try {
             $modules = $client->executeRaw('module', 'list') ?? [];
         } catch (\Throwable $exception) {
-            if (strpos($exception->getMessage(), 'unknown command') === false) {
+            if (strpos($exception->getMessage(), 'unknown command') !== false) {
                 throw $exception;
             }
             $modules = [];
